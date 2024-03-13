d1bc45f1fba3a6fbba524a8df4f21e10987c8bc5
laxmii85 <161963374+laxmii85@users.noreply.github.com>
2024-03-13 12:30:42 +0000

    container config done

---
+++ .devcontainer/01_basic/01_varible.js
@@ -0,0 +1,12 @@
+const accountId=1245;
+let emailId="abc123@gmail.com";
+var accountPassword="456789"; //not use the var keyword bcz here you can face a problem in scopping .
+accountCity ="ahmedabad";
+ //console.log([accountId,emailId accountPassword, accountCity ]);
+
+ //accountId=2345; you can't cahnge the value of const varible.
+
+ emailId="xyz456@gmail.com";
+ accountPassword="789456";
+ accountCity="mumbai";
+ console.log([emailId,accountPassword,accountCity]);
